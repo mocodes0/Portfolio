0094ebb5d45ad54a65d357af9441a1f47abb010b
Mohamed Bashir <Moe@MacBook-Air.local>
2022-06-23 04:23:13 -0400

    Updates

---
+++ precache-manifest.68dbdadde0d4063b778df92c7b5c5551.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a81936f66bd2136d4937debf62068ea",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "7ceee1d9ac1a93d4a0d3",
+    "url": "/Portfolio/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "1a65a7da1e8939f3bc20",
+    "url": "/Portfolio/static/js/2.0e128136.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Portfolio/static/js/2.0e128136.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ceee1d9ac1a93d4a0d3",
+    "url": "/Portfolio/static/js/main.1a5d140d.chunk.js"
+  },
+  {
+    "revision": "87bcb24a22a073d38476",
+    "url": "/Portfolio/static/js/runtime-main.d4446722.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/Portfolio/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/Portfolio/static/media/resume.9d3da0e2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.523de69517b9bce4af8fc92517ea0051.js"
+  "/Portfolio/precache-manifest.68dbdadde0d4063b778df92c7b5c5551.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
